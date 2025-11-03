0d95a5098ed2fbdd2bc510b13b835edbe2526cbd
MD. Sarwar Jahan <Shohidmax@gmail.com>
2025-11-03 20:04:46 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBwIwoj_CgR_lWhFDMlUqOFZFOFIzJu92Q",
+  authDomain: "pran-pos.firebaseapp.com",
+  projectId: "pran-pos",
+  storageBucket: "pran-pos.firebasestorage.app",
+  messagingSenderId: "232805801170",
+  appId: "1:232805801170:web:b2c1c82d95695c00b2e10b",
+  measurementId: "G-11MCYPX70E"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, analytics };
